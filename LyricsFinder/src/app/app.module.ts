080775d56b1934f077f9701b1418c18eb7e2a2c4
Matthew Augustine <matthew.augustine@headoffice.dariel-solutions>
2020-02-14 20:55:08 +0200

    removed unused files

--- LyricsFinder/src/app/app.module.ts
+++ LyricsFinder/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FindlyricsComponent } from './findlyrics/findlyrics.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -15,8 +14,7 @@ import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FindlyricsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
