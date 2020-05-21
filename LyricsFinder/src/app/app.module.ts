1e2c2351697ef506671fbdaef65b9174b74b7e73
Matthew Augustine <matthew.augustine@dariel.co.za>
2020-05-21 15:44:40 +0200

    modified app module

--- LyricsFinder/src/app/app.module.ts
+++ LyricsFinder/src/app/app.module.ts
@@ -2,18 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule, MatProgressSpinnerModule, MatToolbarModule } from '@angular/material';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LyricsComponent } from './lyrics/lyrics.component';
 import { SearchHistoryComponent } from './search-history/search-history.component';
 import { HeaderComponent } from './header/header.component';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
@@ -28,13 +25,8 @@ import { HeaderComponent } from './header/header.component';
     RouterModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    MatExpansionModule,
-    MatCardModule,
-    MatProgressSpinnerModule,
-    MatToolbarModule
+    AngularMaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
